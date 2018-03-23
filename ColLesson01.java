9978581a5f205bf907aa4e7408a83dac54928c65
katsutoshioya <seeds.of.joy0217@gmail.com>
2018-03-23 23:30:54 +0900

    no message

---
+++ ColLesson01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ColLesson01 {
+    public static void main(String[] args) {
+        // Wordクラスのインスタンス配列
+        Word[] words = new Word[10];
+
+        // コマンドラインから入力
+        System.out.println("わからない単語とその意味をスペースで区切って入力して下さい。");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        // ここから記述してください
+        // 例えば、「Dukeくん Javaのマスコットキャラクター」と入力されたときはtmp[0]に"Dukeくん"、tmp[1]に"Javaのマスコットキャラクター"が入る
+        tmp = input.split(" ");
+
+        for(int i=0; i<words.length; i++){
+            System.out.println(words[i]);
+        }
+
+    }
+}
---
+++ Word.java
@@ -0,0 +1,8 @@
+public class Word {
+    public String words;
+    public String tmp;
+
+    public Word(String words) {
+        this.words = words;
+    }
+}
